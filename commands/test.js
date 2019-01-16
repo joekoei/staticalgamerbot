e5ee6a64ae5c2d9cbbec7650ba0da6c805964ca1
Joey Blommers <123118@marklandcollege.nl>
2019-01-16 09:40:49 +0100

    Test.js updated with json file in it

--- commands/test.js
+++ commands/test.js
@@ -2,10 +2,14 @@ const discord = require("discord.js");
 const config = require("../botconfig.json");
 
 module.exports.run => {
+	
+	var warns = require("../warns.json")
+
+	fs.readdir(warns)
 
 }
 
 
 module.exports.help = {
-    name: "ping"
+    name: "test"
 }
